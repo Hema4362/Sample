cd263f717c8ee6cc450c5e13d53e03f635c2fb4d
Hema4362 <hemalatha.g2712@gmail.com>
2020-01-10 12:24:45 +0530

    new code added

--- src/org/cts/Sample.java
+++ src/org/cts/Sample.java
@@ -4,6 +4,13 @@ public class Sample {
 	private void example() {
 		System.out.println("qenl");
 		System.out.println("skvjd");
+		
 
 	}
+	public static void main(String[] args) {
+		Sample s=new Sample();
+		s.example();
+		
+	}
 }
+
